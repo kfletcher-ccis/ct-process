f05b2b9835c564ade996e0ba1f6a164c5d8b7108
kfletcher-ccis <kfletcher@ccis.edu>
2026-07-16 13:44:04 -0500

    Add files via upload

---
+++ loaders/core_loader.py
@@ -0,0 +1,4 @@
+from loaders.base_loader import load_csv
+
+def load_core(file_path: str):
+    return load_csv(file_path)
---
+++ loaders/military_loader.py
@@ -0,0 +1,4 @@
+from loaders.base_loader import load_csv
+
+def load_military(file_path: str):
+    return load_csv(file_path)
